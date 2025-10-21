5c69667957bc6f440478cbb85f683449723492b0
Fovir <fovir@disroot.org>
2025-10-21 22:45:49 +0800

    feat: Add postgres.

---
+++ machines/fovirbox/modules/services/postgres.nix
@@ -0,0 +1,34 @@
+{pkgs, ...}: {
+  virtualisation.oci-containers.containers."postgres" = {
+    image = "postgres";
+
+    environment = import ../../values/postgres-environment.nix;
+
+    volumes = [
+      "postgres:/var/lib/postgresql/data:rw"
+      "/etc/timezone:/etc/timezone"
+      "/etc/localtime:/etc/localtime"
+    ];
+
+    ports = [
+      "127.0.0.1:5432:5432"
+    ];
+  };
+
+  systemd.services.auto-create-user-db = {
+    wantedBy = ["multi-user.target"];
+    after = ["podman-postgres.service"];
+    # requires = ["podman-postgres.service"];
+    description = "Initialize PostgreSQL users and databases";
+    # Without this line, it would Error: configure storage: the 'zfs' command is not available: prerequisites for driver not satisfied (wrong filesystem?)
+    path = with pkgs; [zfs];
+    serviceConfig = {
+      Type = "oneshot";
+      SuccessExitStatus = "0 1";
+      ExecStart = ''
+        ${pkgs.podman}/bin/podman exec -i postgres \
+        psql -U postgres
+      '';
+    };
+  };
+}
