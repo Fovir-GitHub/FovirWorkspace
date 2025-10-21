4bd6723de368ff1e4174ec90e1579c68bc9bf7b0
Fovir <fovir@disroot.org>
2025-10-21 18:06:26 +0800

    feat: Configure the network.

---
+++ machines/fovirbox/modules/network.nix
@@ -0,0 +1,16 @@
+{lib, ...}: {
+  networking = {
+    useDHCP = lib.mkForce false;
+    interfaces.ens3 = import ../values/interfaces.nix;
+
+    defaultGateway = {
+      address = import ../values/ipv4-gateway-address.nix;
+      interface = "ens3";
+    };
+
+    defaultGateway6 = {
+      address = import ../values/ipv6-gateway-address.nix;
+      interface = "ens3";
+    };
+  };
+}
