45fde9fabdcfa0ef2708005d3f697a0984c03460
Fovir <fovir@disroot.org>
2025-10-21 23:59:48 +0800

    chore: Encrypt values.

--- machines/fovirbox/values/ipv4-gateway-address.nix
+++ machines/fovirbox/values/ipv4-gateway-address.nix
@@ -1,14 +1,14 @@
 {
-	"data": "ENC[AES256_GCM,data:EDihu6GS5J+0G/QAU94ZzQ==,iv:6JCH2JVtYZkLN3aC1m+Aaogza4BEH+CZ17Du43xF4w0=,tag:LOGcxDOIC3txjLtrAKVx0g==,type:str]",
+	"data": "ENC[AES256_GCM,data:SV5AMwrWSHqG+P3WONoAMg==,iv:8XNoVqIqkEaUL8NS2r/WFhB4uWXrIAZaKqNKli2AeBY=,tag:6Kagzh1KpBhY07TIAIHGNg==,type:str]",
 	"sops": {
 		"age": [
 			{
 				"recipient": "age172xqa230e5vwzsdajc6ak235qvg448nf9g02xjp80w3vknsacd3qszzc87",
-				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBaQkkvM240OGlrTDBRclp0\nZ2VzWS9JaEZ0UHJzZVU1ak1HamtSUVExdnc4Cnh5ZjhqYTRtbngwWGlWWUVSek9h\nZmpXL3I1bW1tYlRINWVvbDlkR0JqZWsKLS0tIHQ0aXFQLy9GU0hmTFRGVlZKN3o5\nRUx2NjR4NDBlcUk4WHpjWWh1NVdiMjgKLTfmyCnU/Y4pJOTZPtzJUxp+OCY+OpsH\n2/B2qiAN6ttck03RbkBV6NCxZrZGvLUgM33D5gXy4jhoFql0oa0OAw==\n-----END AGE ENCRYPTED FILE-----\n"
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBTTURrYnZ0L3NFRngxcHRG\nWGNOM1V0UjI5eUFuVndvL3h0Zm5OWjFSejMwCkxRQVZEc0N0STROSmFreERDSU1k\ncWMycHAyclBZeUEwbWo4OWRSUDk5cTgKLS0tIDYycGNmVWRmaXJmbmhMZHZ5b0tV\ndUtTMCs4eHRjSDNiZWZ0ZVhud0xFMGMKDEM6DCnpLYa148PmrCxuwXPRGX/Suezs\njV6HD8NoYw2gmnls6bIxTKixkPu+Ub9ydJ+wC05DEdpxVuquNDUUbA==\n-----END AGE ENCRYPTED FILE-----\n"
 			}
 		],
-		"lastmodified": "2025-10-21T15:55:33Z",
-		"mac": "ENC[AES256_GCM,data:+TJJwvbWFxgAPmiBR8l1zKDCJkuB9frzwWJImzveL3ntmT3Sg5m3SK1qRcK2RpSv9jwGUI5vVf9GvB0ZZZVd/C/ub96GAxR+P03wkiswk4DNYvQVHpvAgM36vuEjKWqo4QR70iuv44ezCytLv/pR1S+ykAspUdzRnJEVaW4PktI=,iv:l2jOxPA0/Lw3rfeMQFHKgEy3A/PEwdsWCR/hdYhaNik=,tag:bQrhVOHwjo4CKmmJjEH3Kw==,type:str]",
+		"lastmodified": "2025-10-21T15:59:48Z",
+		"mac": "ENC[AES256_GCM,data:XqhXu8ERIx1Ih3MvwmpxMdUG6lbBEnrQ39KPCegtmpFaNawCo9OwLGzbGei6ZLMkURaE5+Ms5/5w8TuAiQaxnuOa0CtQ86bez9D4IEh8Bq8eXEZmH5I0UF09FmmXPVuYlznTvb/4hWstrdjLmxpXJauO4XakwXKoSyVLogvqSW0=,iv:978cuG23IykY+voeMhV8Vz9A1GEgSIBuz8760oOO6uU=,tag:9E2c4W6+HsLE6f4oykoahA==,type:str]",
 		"version": "3.11.0"
 	}
 }
--- machines/fovirbox/values/ssh-port.nix
+++ machines/fovirbox/values/ssh-port.nix
@@ -1,14 +1,14 @@
 {
-	"data": "ENC[AES256_GCM,data:CU40I7QJ6ugnv0A=,iv:ibCM57vF1al1/KwMSoG6c8OVuppwAhRC5Bd+s+qflOo=,tag:ycP0QANq5AgaIHn3Vavmyw==,type:str]",
+	"data": "ENC[AES256_GCM,data:9HsKqD/SNMmUKqU=,iv:Av1CPljuuAJwqeqM/cXxB3qeT4RyVqtdP5oWArj9Wnw=,tag:7Oysia15l+t7pb8E69gjqQ==,type:str]",
 	"sops": {
 		"age": [
 			{
 				"recipient": "age172xqa230e5vwzsdajc6ak235qvg448nf9g02xjp80w3vknsacd3qszzc87",
-				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBmR1BVVHQ5RnpSZkhLNS9y\nZ3hLWktEV28vU3RTL04rTkVLNWJLbzJiZ1NBClpUT3Jycm4rYUlESHJxTmJEcHMv\nN1pDaEV0QTJFZE1WbTV4bm9WdUd4SG8KLS0tIE40RWt5RWVBWjFjMUloTWxOZ0hw\nNnlNR25ybU1ROTMyOGVteVNPbDN5ZEUK/8HAcDvt7u0/pcrx6XlOIRhlCM+ygAkY\n6APJYOE5JxzsCSuzve5h11SeMKKnHAiro9US0GdwbEnHZXgc+/PW3Q==\n-----END AGE ENCRYPTED FILE-----\n"
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBLSnExY0tkY2VOZVBUV1Nm\nZDVNQndqTW1zUTdrcXBqM01EOG44ZzlYU0RnCmhnQUFsSTRobVgrMWQvSlZ0OTNh\nUHE4ek0yVmF5QkdubjE5R0o4TndYZUUKLS0tIFpHRWdONVZLbjRNN3pGc294U0Rz\nTmtnMVV6aHJUVEtGdG5zeUMvYUZJbHcKTAIfceC8acJy2+NEN/vl7LiXtBeVBoVn\nuH66g8APmnAFCrS8oMTpjH0dScvSCDhA82Xr10OEWmxC/86BaXLVJA==\n-----END AGE ENCRYPTED FILE-----\n"
 			}
 		],
-		"lastmodified": "2025-10-21T15:55:33Z",
-		"mac": "ENC[AES256_GCM,data:vpSmfZf/uqbkXL7pYk7wFp9cmDJ9VgRdCs82HCcxM3/Pw4RcVhe+iE4Tx4OTB0EaY52dF8goRXaq3Q/J+gHpf3+rmN5NJOq77YAan9b3fZOxXCk4uoearYSvWZu8er4FKQpxtt7gsHzXLqt2BBa9J/0Z5Z//Z3IXbAPsIL5r1Q0=,iv:CT5OzHbBtkIVIix97v3XGS9Ai1dzdax//WHs639U8xw=,tag:BffopEmcCkc+e5W1TUq15A==,type:str]",
+		"lastmodified": "2025-10-21T15:59:48Z",
+		"mac": "ENC[AES256_GCM,data:sv1k4ILIARfA56uSAplFWj/LFtV636oWH6norVSDhp4xHUxsxjaIuTBmXUKO3tyCv9rY35ZVc7+ED6UsAYXfqxlQZbp7AvMcQzHO+zCjAK/KJEIjUuFQCnoQiPcqW9IyI8KBy85AkvAgnuN4TbzEuTX5jAi8gvM4Uo87Z+vXkJQ=,iv:6PM7apWxrS1sM98Yg7AbcikB/pyRneMXwMWuwNydPu8=,tag:GflcII2x8tReROW1ADAn5Q==,type:str]",
 		"version": "3.11.0"
 	}
 }
