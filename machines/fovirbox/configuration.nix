28b2b631643b6dadbb6a4deeef024cd249aada40
Fovir <fovir@disroot.org>
2025-10-21 18:07:16 +0800

    refactor: Set ssh port.

--- machines/fovirbox/configuration.nix
+++ machines/fovirbox/configuration.nix
@@ -2,6 +2,8 @@
   disko.devices.disk.main.device = "/dev/disk/by-path/pci-0000:00:07.0";
   networking.hostId = "00000000";
 
+  services.openssh.ports = import ./values/ssh-port.nix;
+
   imports = [
     ./modules
     ../../modules/optional/zfs.nix
