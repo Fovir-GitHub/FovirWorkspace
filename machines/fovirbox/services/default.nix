719b3705bea8d60b25180ba77279a9a04e93b039
Fovir <fovir@disroot.org>
2025-10-23 16:47:40 +0800

    feat: Add `umami`.

--- machines/fovirbox/services/default.nix
+++ machines/fovirbox/services/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
+    ./umami.nix
     ./restic.nix
     ./wakapi.nix
     ./postgres.nix
---
+++ machines/fovirbox/services/umami.nix
@@ -0,0 +1,9 @@
+{...}: {
+  virtualisation.oci-containers.containers."umami" = {
+    image = "ghcr.io/umami-software/umami:postgresql-latest";
+    ports = [
+      "127.0.0.1:4000:3000"
+    ];
+    environment = import ../values/umami-env.nix;
+  };
+}
