8e5d57e0ec5f351dcb8d945673b0b8f492b64e41
Fovir <fovir@disroot.org>
2025-10-24 14:35:19 +0800

    feat: Migrate the `desktop` module.

---
+++ machines/foviros/modules/desktop/default.nix
@@ -0,0 +1,7 @@
+{
+  imports = [
+    ./hyprland.nix
+    ./fcitx5.nix
+    ./fonts.nix
+  ];
+}
---
+++ machines/foviros/modules/desktop/fcitx5.nix
@@ -0,0 +1,16 @@
+{pkgs, ...}: {
+  i18n.inputMethod = {
+    type = "fcitx5";
+    enable = true;
+
+    fcitx5 = {
+      addons = with pkgs; [
+        fcitx5-gtk
+        fcitx5-rime
+        libsForQt5.fcitx5-qt
+      ];
+
+      waylandFrontend = true;
+    };
+  };
+}
