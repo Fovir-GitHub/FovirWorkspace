b194e7c6ce1dd520b6fc90111b8dcd1307868cff
Fovir <fovir@disroot.org>
2025-10-25 19:47:44 +0800

    feat: Add s3 backup.

--- machines/fovirbox/services/restic.nix
+++ machines/fovirbox/services/restic.nix
@@ -2,4 +2,11 @@
   environment.systemPackages = with pkgs; [
     restic
   ];
+
+  environment.etc = import ../values/restic-etc.nix;
+  services.restic = {
+    backups = {
+      s3 = import ../values/restic-s3.nix;
+    };
+  };
 }
