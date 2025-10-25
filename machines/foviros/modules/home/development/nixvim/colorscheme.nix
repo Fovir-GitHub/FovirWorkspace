1cf1d74d559b22becb276ec08810a564751b1007
Fovir <fovir@disroot.org>
2025-10-25 23:55:45 +0800

    style: Remove unused code.

--- machines/foviros/modules/home/development/nixvim/colorscheme.nix
+++ machines/foviros/modules/home/development/nixvim/colorscheme.nix
@@ -20,15 +20,4 @@
       };
     };
   };
-
-  # extraConfigLuaPost = ''
-  #   local cp = require("catppuccin.palettes").get_palette("mocha")
-  #
-  #   vim.api.nvim_set_hl(0, "NormalFloat", { bg = cp.base })
-  #   vim.api.nvim_set_hl(0, "FloatBorder", { bg = cp.base, fg = cp.surface1 })
-  #   vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = cp.base })
-  #   vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = cp.base })
-  #   vim.api.nvim_set_hl(0, "Pmenu", { bg = cp.base })
-  #   vim.api.nvim_set_hl(0, "PmenuSel", { bg = cp.surface1 })
-  # '';
 }
