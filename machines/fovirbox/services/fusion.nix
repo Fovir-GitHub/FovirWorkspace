147451c09525cddf2ad9ea7d293e875b216951fb
Fovir <fovir@disroot.org>
2025-11-01 16:29:51 +0800

    feat: Use my own image.

--- machines/fovirbox/services/fusion.nix
+++ machines/fovirbox/services/fusion.nix
@@ -1,6 +1,6 @@
 {...}: {
   virtualisation.oci-containers.containers."fusion" = {
-    image = "ghcr.io/0x2e/fusion:latest";
+    image = "ghcr.io/fovir-github/fusion:main";
     ports = [
       "127.0.0.1:8080:8080"
     ];
