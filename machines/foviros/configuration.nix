a3ef7cfe3d39a834fca49a6cd4b2bc5d4c54dc34
Fovir <fovir@disroot.org>
2025-11-04 23:17:44 +0800

    feat: Enable dot.

--- machines/foviros/configuration.nix
+++ machines/foviros/configuration.nix
@@ -4,18 +4,16 @@
   imports = [
     ../../modules/optional/boot-loader/systemd-boot.nix
     ../../modules/optional/desktop-services
+    ../../modules/optional/dns.nix
     ../../modules/optional/kernels/cachyos-desktop.nix
-    ../../modules/optional/virtualisation/qemu.nix
     ../../modules/optional/virtualisation/podman.nix
+    ../../modules/optional/virtualisation/qemu.nix
     ../../modules/optional/zfs.nix
     ../../modules/shared
     ./modules
     ./values/hardware-configuration.nix
     inputs.chaotic.nixosModules.default
-    inputs.daeuniverse.nixosModules.dae
-    inputs.daeuniverse.nixosModules.daed
     inputs.home-manager.nixosModules.home-manager
-
     {
       nixpkgs.overlays = [
         inputs.nix-vscode-extensions.overlays.default
