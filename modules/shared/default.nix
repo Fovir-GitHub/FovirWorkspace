09569e4b5ad829277e0d14b5c95e1910a3464469
Fovir <fovir@disroot.org>
2025-11-12 09:02:43 +0800

    feat: Add overlay of `hyprpanel`.

--- modules/shared/default.nix
+++ modules/shared/default.nix
@@ -3,6 +3,7 @@
     ./bootstrap.nix
     ./build-vm-settings.nix
     ./nix.nix
+    ./overlays.nix
     ./ssh.nix
   ];
 }
---
+++ modules/shared/overlays.nix
@@ -0,0 +1,16 @@
+{pkgs, ...}: {
+  nixpkgs = {
+    overlays = [
+      (final: prev: {
+        hyprpanel = prev.hyprpanel.overrideAttrs (old: {
+          src = pkgs.fetchFromGitHub {
+            owner = "Fovir-GitHub";
+            repo = "HyprPanel";
+            rev = "839f05f262ae654e5e4fff2e07cf45bfa6f54c85";
+            hash = "sha256-YNEht+K7DFGzwoELE1LCSHJ4Vj8dazUBLYGs5hAFBpI=";
+          };
+        });
+      })
+    ];
+  };
+}
