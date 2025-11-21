f0f3c663700b6cfd0a2661aeccbe2237541dad6b
Fovir <fovir@disroot.org>
2025-11-21 14:15:33 +0800

    feat: disable hyprshell overview

--- machines/foviros/modules/home/desktop/hyprshell/default.nix
+++ machines/foviros/modules/home/desktop/hyprshell/default.nix
@@ -18,32 +18,7 @@
         enable = true;
 
         items_per_row = 5;
-        overview = {
-          enable = true;
-
-          filter_by = ["current_workspace"];
-          hide_filtered = false;
-          key = "super_l";
-          launcher = {
-            launch_modifier = "alt";
-            max_items = 6;
-            plugins = {
-              actions = {enable = false;};
-              applications = {
-                enable = true;
-
-                show_actions_submenu = false;
-                show_execs = false;
-              };
-              terminal = {enable = false;};
-              websearch = {enable = false;};
-              calc = {enable = false;};
-            };
-            show_when_empty = false;
-            width = 650;
-          };
-          modifier = "super";
-        };
+        overview.enable = false;
         scale = 8.5;
         switch = {
           enable = true;
