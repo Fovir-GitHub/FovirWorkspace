96613a6197ee37413ec42d468ad79630c95d2341
Fovir <fovir@disroot.org>
2025-11-22 16:33:01 +0800

    refactor: use `xdg.configFile`

--- machines/foviros/modules/home/terminal/fastfetch/default.nix
+++ machines/foviros/modules/home/terminal/fastfetch/default.nix
@@ -1,11 +1,8 @@
-{config, ...}: {
-  programs.fastfetch = {
-    enable = true;
-  };
+{...}: {
+  programs.fastfetch.enable = true;
 
-  home.file."${config.xdg.configHome}/fastfetch/config.jsonc".source = ./fastfetch.jsonc;
-
-  home.file."${config.xdg.configHome}/fastfetch/logo.txt" = {
-    source = ./logo.txt;
+  xdg.configFile = {
+    "fastfetch/config.jsonc".source = ./fastfetch.jsonc;
+    "fastfetch/logo.txt".source = ./logo.txt;
   };
 }
