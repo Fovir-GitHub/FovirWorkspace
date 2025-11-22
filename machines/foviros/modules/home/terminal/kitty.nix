8ecbb50c064994f047221664aca5f1d36a156e03
Fovir <fovir@disroot.org>
2025-11-22 14:22:08 +0800

    style: format code

--- machines/foviros/modules/home/terminal/kitty.nix
+++ machines/foviros/modules/home/terminal/kitty.nix
@@ -2,28 +2,21 @@
   programs.kitty = {
     enable = true;
 
+    enableGitIntegration = true;
+    font = {
+      name = "JetBrainsMono Nerd Font Mono";
+      size = 14;
+    };
+    keybindings = {
+      "ctrl+shift+c" = "copy_to_clipboard";
+      "ctrl+shift+v" = "paste_from_clipboard";
+    };
+    settings.clear_all_shortcuts = "yes";
     shellIntegration = {
       enableBashIntegration = true;
       enableZshIntegration = true;
       mode = "no-rc";
     };
-
-    enableGitIntegration = true;
-
-    font = {
-      name = "JetBrainsMono Nerd Font Mono";
-      size = 14;
-    };
-
     themeFile = "Catppuccin-Mocha";
-
-    settings = {
-      clear_all_shortcuts = "yes";
-    };
-
-    keybindings = {
-      "ctrl+shift+c" = "copy_to_clipboard";
-      "ctrl+shift+v" = "paste_from_clipboard";
-    };
   };
 }
