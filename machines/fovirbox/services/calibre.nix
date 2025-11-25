511df2dfd525441837b406099cc7808700974f74
Fovir <fovir@disroot.org>
2025-11-26 01:41:06 +0800

    refactor: remove `calibre-library-path`

--- machines/fovirbox/services/calibre.nix
+++ machines/fovirbox/services/calibre.nix
@@ -3,8 +3,8 @@
     image = "lscr.io/linuxserver/calibre-web:latest";
     environment = import ../values/calibre-env.nix;
     volumes = [
-      "${import ../values/podman-data-dir.nix}/calibre:/config"
-      "${import ../values/calibre-library-path.nix}:/books"
+      "${import ../values/podman-data-dir.nix}/calibre/books:/books"
+      "${import ../values/podman-data-dir.nix}/calibre/config:/config"
     ];
     ports = ["127.0.0.1:8083:8083"];
   };
