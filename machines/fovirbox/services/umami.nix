987b6a7c0f799b985dd90ec6e4e3015c5aa5fff2
Fovir <fovir@disroot.org>
2025-12-06 19:02:51 +0800

    chore: upgrade `umami` to fix CVE

--- machines/fovirbox/services/umami.nix
+++ machines/fovirbox/services/umami.nix
@@ -1,6 +1,7 @@
 {...}: {
   virtualisation.oci-containers.containers."umami" = {
     image = "ghcr.io/umami-software/umami:postgresql-latest";
+    pull = "newer";
     ports = ["127.0.0.1:4000:3000"];
     environment = import ../values/umami-env.nix;
   };
