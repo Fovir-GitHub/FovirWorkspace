503e3260ca10193878f7c0fbd012357202d4aee9
Fovir <fovir@disroot.org>
2026-01-07 23:32:00 +0800

    feat: override the rule of surrounds

--- machines/foviros/modules/home/development/nixvim/plugins/nvim-surround.nix
+++ machines/foviros/modules/home/development/nixvim/plugins/nvim-surround.nix
@@ -2,6 +2,47 @@
   plugins.nvim-surround = {
     enable = true;
 
-    settings.keymaps.visual = "S";
+    settings = {
+      keymaps.visual = "S";
+
+      surrounds = {
+        "(" = {
+          add = ["(" ")"];
+          delete = "^(. ?)().-( ?.)()$";
+          find.__raw = ''
+            function()
+              return M.get_selection({ motion = "a(" })
+            end
+          '';
+        };
+        "[" = {
+          add = ["[" "]"];
+          delete = "^(. ?)().-( ?.)()$";
+          find.__raw = ''
+            function()
+              return M.get_selection({ motion = "a[" })
+            end
+          '';
+        };
+        "{" = {
+          add = ["{" "}"];
+          delete = "^(. ?)().-( ?.)()$";
+          find.__raw = ''
+            function()
+              return M.get_selection({ motion = "a{" })
+            end
+          '';
+        };
+        "<" = {
+          add = ["<" ">"];
+          delete = "^(. ?)().-( ?.)()$";
+          find.__raw = ''
+            function()
+              return M.get_selection({ motion = "a<" })
+            end
+          '';
+        };
+      };
+    };
   };
 }
