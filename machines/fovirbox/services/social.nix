ac8ca80db25cc19a2844d324939052b9ca137de7
Fovir <fovir@disroot.org>
2026-01-13 17:20:30 +0800

    chore: roll back to use `elk`

--- machines/fovirbox/services/social.nix
+++ machines/fovirbox/services/social.nix
@@ -1,16 +1,16 @@
 {...}: {
   virtualisation.oci-containers.containers = {
-    # "elk" = {
-    #   image = "ghcr.io/fovir-github/elk:main";
-    #   ports = ["127.0.0.1:1111:5314"];
-    #   pull = "always";
-    #   volumes = ["${import ../values/podman-data-dir.nix}/elk:/elk/data"];
-    # };
-    "fe" = {
-      image = "ghcr.io/fovir-github/masto-fe-standalone:latest";
-      ports = ["127.0.0.1:1110:80"];
+    "elk" = {
+      image = "ghcr.io/fovir-github/elk:main";
+      ports = ["127.0.0.1:1111:5314"];
       pull = "always";
+      volumes = ["${import ../values/podman-data-dir.nix}/elk:/elk/data"];
     };
+    # "fe" = {
+    #   image = "ghcr.io/fovir-github/masto-fe-standalone:latest";
+    #   ports = ["127.0.0.1:1110:80"];
+    #   pull = "always";
+    # };
     "gotosocial" = {
       environment = import ../values/gotosocial-env.nix;
       image = "docker.io/superseriousbusiness/gotosocial:latest";
