fef606099a9cadd23beedd3cc8b225b838ca8241
Fovir <fovir@disroot.org>
2026-01-13 15:54:13 +0800

    feat: use `fe` instead of `elk`

--- machines/fovirbox/services/social.nix
+++ machines/fovirbox/services/social.nix
@@ -1,10 +1,15 @@
 {...}: {
   virtualisation.oci-containers.containers = {
-    "elk" = {
-      image = "ghcr.io/fovir-github/elk:main";
-      ports = ["127.0.0.1:1111:5314"];
+    # "elk" = {
+    #   image = "ghcr.io/fovir-github/elk:main";
+    #   ports = ["127.0.0.1:1111:5314"];
+    #   pull = "always";
+    #   volumes = ["${import ../values/podman-data-dir.nix}/elk:/elk/data"];
+    # };
+    "fe" = {
+      image = "ghcr.io/fovir-github/masto-fe-standalone:latest";
+      ports = ["127.0.0.1:1110:80"];
       pull = "always";
-      volumes = ["${import ../values/podman-data-dir.nix}/elk:/elk/data"];
     };
     "gotosocial" = {
       environment = import ../values/gotosocial-env.nix;
