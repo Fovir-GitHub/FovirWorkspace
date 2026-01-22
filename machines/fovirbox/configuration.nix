d48afda928004bbdbdc646c267ae91b2cdabb6a7
Fovir <fovir@disroot.org>
2026-01-22 19:46:42 +0800

    feat: migrate to new machine

--- machines/fovirbox/configuration.nix
+++ machines/fovirbox/configuration.nix
@@ -1,6 +1,9 @@
-{...}: {
+{lib, ...}: let
+  disk = "/dev/disk/by-path/virtio-pci-0000:00:07.0";
+in {
   boot.kernelParams = ["zfs.zfs_arc_max=2147483648"];
-  disko.devices.disk.main.device = "/dev/disk/by-path/pci-0000:00:07.0";
+  boot.loader.limine.biosDevice = lib.mkForce disk;
+  disko.devices.disk.main.device = disk;
   networking.hostId = "00000000";
   services.openssh.ports = import ./values/ssh-port.nix;
   users.users.root = import ./values/user-root.nix;
--- machines/fovirbox/modules/network.nix
+++ machines/fovirbox/modules/network.nix
@@ -1,10 +1,14 @@
 {lib, ...}: {
   networking = {
+    useDHCP = lib.mkForce false;
+    interfaces.ens3 = import ../values/interfaces.nix;
     defaultGateway = {
       address = import ../values/ipv4-gateway-address.nix;
       interface = "ens3";
     };
-    interfaces.ens3 = import ../values/interfaces.nix;
-    useDHCP = lib.mkForce false;
+    defaultGateway6 = {
+      address = import ../values/ipv6-gateway-address.nix;
+      interface = "ens3";
+    };
   };
 }
