a9afd9b7794c3ea8674c84e1217a7e25f0156c7b
Fovir <fovir@disroot.org>
2026-01-24 15:37:50 +0800

    feat: add overlay of `hlchunk`

--- modules/shared/overlays.nix
+++ modules/shared/overlays.nix
@@ -1,9 +1,27 @@
-{inputs, ...}: {
+{
+  inputs,
+  pkgs,
+  ...
+}: {
   nixpkgs.overlays = [
     (_final: prev: {
       sui = prev.callPackage ../../pkgs/sui {
         platform = prev.stdenv.hostPlatform;
       };
+    })
+    (final: prev: {
+      vimPlugins =
+        prev.vimPlugins
+        // {
+          hlchunk-nvim = prev.vimPlugins.hlchunk-nvim.overrideAttrs (oldAttrs: {
+            src = pkgs.fetchFromGitHub {
+              owner = "shellRaining";
+              repo = "hlchunk.nvim";
+              rev = "d5e45809ed93991ade8e10e4f706cd7699b17430";
+              sha256 = "sha256-NcfH9AnURII+MTSoyDgnotpzjcLNxXx1BXnKYXP2C/0=";
+            };
+          });
+        };
     })
     inputs.nix-vscode-extensions.overlays.default
   ];
