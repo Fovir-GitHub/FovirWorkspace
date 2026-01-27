e4341e9e3cef15fabe77d2bd91c196f2642ea25a
Fovir <fovir@disroot.org>
2026-01-27 17:06:14 +0800

    fix: disable the pop up message of `hop`

--- machines/foviros/modules/home/development/nixvim/plugins/noice.nix
+++ machines/foviros/modules/home/development/nixvim/plugins/noice.nix
@@ -6,6 +6,15 @@
       settings = {
         lsp.enabled = true;
         presets.command_palette = true;
+        routes = [
+          {
+            filter = {
+              event = "msg_show";
+              any = [{find = "Hop";} {find = "hop";}];
+            };
+            opts = {skip = true;};
+          }
+        ];
         views.cmdline_popup.size = {
           width = "50%";
           height = "auto";
