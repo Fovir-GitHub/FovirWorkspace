24400d5b3043333e4e4132c22e434f6bda9f74bb
Fovir <fovir@disroot.org>
2026-02-14 14:31:39 +0800

    feat: enable catppuccin theme for fcitx5

--- modules/optional/fcitx5.nix
+++ modules/optional/fcitx5.nix
@@ -20,28 +20,43 @@
       "fcitx5-remote -r"
     ];
 
-    xdg = {
-      configFile."fcitx5/conf/rime.conf".text = ''
-        # Preedit Mode
-        PreeditMode="Inline"
-        # Fix embedded preedit cursor at the beginning of the preedit
-        PreeditCursorPositionAtBeginning=False
-        # Action when switching input method
-        # SwitchInputMethodBehavior="Commit commit preview"
-        # Deploy
-        Deploy=
-        # Synchronize
-        Synchronize=
-      '';
-      dataFile."fcitx5/rime" = {
-        force = true;
-        recursive = true;
-        source = pkgs.fetchFromGitHub {
-          hash = "sha256-Y6/tU63+JQ9HX1m/kI9VQz6tIhFVRAPSsp6Vf47gzUk=";
-          owner = "iDvel";
-          repo = "rime-ice";
-          rev = "23f0c39a0b443524e37dbff4f085236b32691291";
+    xdg = let
+      ctpThemes = pkgs.fetchFromGitHub {
+        owner = "Fovir-GitHub";
+        repo = "ctp-fcitx5";
+        rev = "main";
+        sha256 = "sha256-Hhd+V7b5Wp7+SvImNdFk1TjiFUZp+araBxjCpIdi9sQ=";
+      };
+    in {
+      configFile = {
+        "fcitx5/conf/classicui.conf".text = ''
+          Theme=catppuccin-mocha-lavender
+        '';
+        "fcitx5/conf/rime.conf".text = ''
+          # Preedit Mode
+          PreeditMode="Inline"
+          # Fix embedded preedit cursor at the beginning of the preedit
+          PreeditCursorPositionAtBeginning=False
+          # Action when switching input method
+          # SwitchInputMethodBehavior="Commit commit preview"
+          # Deploy
+          Deploy=
+          # Synchronize
+          Synchronize=
+        '';
+      };
+      dataFile = {
+        "fcitx5/rime" = {
+          force = true;
+          recursive = true;
+          source = pkgs.fetchFromGitHub {
+            hash = "sha256-Y6/tU63+JQ9HX1m/kI9VQz6tIhFVRAPSsp6Vf47gzUk=";
+            owner = "iDvel";
+            repo = "rime-ice";
+            rev = "23f0c39a0b443524e37dbff4f085236b32691291";
+          };
         };
+        "fcitx5/themes".source = "${ctpThemes}/src";
       };
     };
   };
