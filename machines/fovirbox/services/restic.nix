4060c1064be5824ce21da1014c4906787cbbdabd
Fovir <fovir@disroot.org>
2026-02-20 13:42:28 +0800

    refactor: put all restic configuration in the value

--- machines/fovirbox/services/restic.nix
+++ machines/fovirbox/services/restic.nix
@@ -1,6 +1,4 @@
 {pkgs, ...}: {
   environment.systemPackages = [pkgs.restic];
-
-  environment.etc = import ../values/restic-etc.nix;
-  services.restic.backups.s3 = import ../values/restic-s3.nix;
+  imports = [../values/restic-conf.nix];
 }
