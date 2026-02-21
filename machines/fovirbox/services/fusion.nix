1d2ea73b3df43273b745e4fa0751c767e32e9484
Fovir <fovir@disroot.org>
2026-02-21 13:28:33 +0800

    feat: use newest `fusion`

--- machines/fovirbox/services/fusion.nix
+++ machines/fovirbox/services/fusion.nix
@@ -2,7 +2,7 @@
   virtualisation.oci-containers.containers."fusion" = {
     autoStart = true;
     environment = import ../values/fusion-env.nix;
-    image = "ghcr.io/fovir-github/fusion:main";
+    image = "ghcr.io/0x2e/fusion:latest";
     ports = ["127.0.0.1:8080:8080"];
     pull = "always";
     volumes = ["${import ../values/podman-data-dir.nix}/fusion:/data"];
