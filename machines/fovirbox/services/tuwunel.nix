628e13b4443c411912449c88574bca0c6253c28b
Fovir <fovir@disroot.org>
2026-05-20 00:23:35 +0800

    chore: remove the resource limitation of `tuwunel`

--- machines/fovirbox/services/tuwunel.nix
+++ machines/fovirbox/services/tuwunel.nix
@@ -3,11 +3,6 @@
     image = "ghcr.io/matrix-construct/tuwunel:latest";
 
     environment = import ../values/tuwunel-env.nix;
-    extraOptions = [
-      "--cpus=0.5"
-      "--memory=256m"
-      "--memory-swap=256m"
-    ];
     ports = ["127.0.0.1:2217:8008"];
     pull = "newer";
     volumes = ["${import ../values/podman-data-dir.nix}/tuwunel:/var/lib/tuwunel"];
