59039f3bfe29b36b8092da00bbd9499982d4f336
Fovir <fovir@disroot.org>
2026-05-22 11:26:09 +0800

    feat: enable geo ip in `liwan`

--- machines/fovirbox/services/liwan.nix
+++ machines/fovirbox/services/liwan.nix
@@ -1,8 +1,16 @@
-{...}: {
+{pkgs, ...}: let
+  geoIP = pkgs.fetchurl {
+    url = "https://downloads.ip66.dev/db/ip66.mmdb";
+    hash = "sha256-qWSi4HgzVEbfYvb/poZrU/ZZeeI67b9Ql45eiitBJBU=";
+  };
+in {
   virtualisation.oci-containers.containers."liwan" = {
     image = "ghcr.io/explodingcamera/liwan:latest";
     ports = ["127.0.0.1:9042:9042"];
-    volumes = ["liwan:/data"];
+    volumes = [
+      "${geoIP}:/geoip/geoip.mmdb"
+      "liwan:/data"
+    ];
     environment = import ../values/liwan-env.nix;
   };
 }
