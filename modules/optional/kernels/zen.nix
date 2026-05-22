b347e9a05f47b7d1b7fd4051c6f091b5991149c6
Fovir <fovir@disroot.org>
2026-05-23 02:51:17 +0800

    chore: remove zen kernel patch

--- modules/optional/kernels/zen.nix
+++ modules/optional/kernels/zen.nix
@@ -1,16 +1,3 @@
 {pkgs, ...}: {
-  boot = {
-    kernelPackages = pkgs.linuxPackages_zen;
-    kernelPatches = [
-      {
-        name = "fix_mediatek_bluetooth";
-        patch = (
-          pkgs.fetchpatch {
-            url = "https://lore.kernel.org/linux-mediatek/20260514-bluetooh-fix-mt7922-v1-1-499c878af1e5@zohomail.in/raw";
-            hash = "sha256-JHUGOYK4Wk3VIIl3nM73YYe+odrTOf5tcLg7ZjGRYGs=";
-          }
-        );
-      }
-    ];
-  };
+  boot.kernelPackages = pkgs.linuxPackages_zen;
 }
