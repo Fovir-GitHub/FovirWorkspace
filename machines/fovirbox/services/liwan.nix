ba746925166347477123a9eb4288cb64e3a3c288
Fovir <fovir@disroot.org>
2026-05-27 23:05:41 +0800

    feat: update the hash value of geoIP
    Signed-off-by: Fovir <fovir@disroot.org>

--- machines/fovirbox/services/liwan.nix
+++ machines/fovirbox/services/liwan.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: let
   geoIP = pkgs.fetchurl {
     url = "https://downloads.ip66.dev/db/ip66.mmdb";
-    hash = "sha256-qWSi4HgzVEbfYvb/poZrU/ZZeeI67b9Ql45eiitBJBU=";
+    hash = "sha256-CRZK2UWCADwVvxAst9kzrjWvRnQQ3/ItvsiV4W17O94=";
   };
 in {
   virtualisation.oci-containers.containers."liwan" = {
